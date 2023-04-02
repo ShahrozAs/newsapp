9da583f7d6b01576b8f0a86b7f9f7598296f8415
ShahrozAs <mr.sherry.793@gmail.com>
2023-04-02 23:03:24 +0500

    Adding loading Animations

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import NewItems from './NewItems'
+import Spinner from './Spinner';
 //let {pagesize}=this.props;
 export class News extends Component {
   
@@ -8,45 +9,54 @@ constructor(){
  this.state={
   articles:[],
   page:1,
+  loading:false
 
  }
 }
 
 async componentDidMount() {
+  this.setState({
+    loading:true
+  });
   const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8c5e5ec518bc4fe29c8f9469a9bd9d1c&page=1&pageSize=${this.props.pagesize}`);
+ 
   const data = await response.json();
-  this.setState({ articles: data.articles,TotalResult:data.totalResult});
+  this.setState({ articles: data.articles,
+    TotalResult:data.totalResult,
+     loading:false
+  });
 }
 handlePrevClick=async()=>{
   console.log("prvious");
+  this.setState({loading:true});
   const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8c5e5ec518bc4fe29c8f9469a9bd9d1c&page=${this.state.page-1}&pageSize=${this.props.pagesize}`);
   const data = await response.json();
   this.setState({
     page:this.state.page-1,
-     articles: data.articles
+     articles: data.articles,
+     loading:false
     })
 }
  handleNextClick=async()=>{
-   if (this.state.page+1>Math.ceil(this.state.TotalResult/this.props.pagesize)) {
-     
-  }
-  else{
+   if (!(this.state.page+1>Math.ceil(this.state.TotalResult/this.props.pagesize)))
     console.log("Next");
+    this.setState({loading:true});
     const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8c5e5ec518bc4fe29c8f9469a9bd9d1c&page=${this.state.page+1}&pageSize=${this.props.pagesize}`);
     const data = await response.json();
     this.setState({
       page:this.state.page+1,
-      articles: data.articles
+      articles: data.articles,
+      loading:false
     })
   }
-}
+
   render() {
     return (
       <div className='container my-3'>
         <h1 className="text-center">NewsMonkey - Top Headlines</h1>
-      
+         {this.state.loading&&<Spinner/>}
         <div className="row my-3" >
-          {this.state.articles.map((element)=>{
+          {!this.state.loading&& this.state.articles.map((element)=>{
 
            return <div className="col-md-4" key={element.url}>
             <NewItems title={element.title} description={element.description} newsImage={element.urlToImage} url={element.url}/>
